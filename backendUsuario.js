8400e6741ab20677d5bab54b96a591538f4a11cc
gabriel48fernandes <gabrielfernandesacdc@gmail.com>
2024-10-25 19:02:14 -0300

    Add files via upload

---
+++ backendUsuario.js
@@ -0,0 +1,20 @@
+ //tipos de cortes
+ let serviços =
+ [    
+     {corte: "corte basico & barba (sem degrade)", valor: 45.00},
+     {corte: "corte basico, barba+sombrancelha", valor: 50.00},
+     {corte: "corte na 0/1 & barba", valor: 50.00 },
+     {corte: "corte degrade & barba + sombrancelha", valor: 55.00},
+     {corte: "corte degrade na shaver & barba", valor: 50.00}
+      
+ ]
+// Matriz de horários disponíveis
+let horariosDisponiveis = [
+    "09:00", "10:00", "11:00", "12:00", "13:00", 
+    "14:00", "15:00", "16:00", "17:00", "18:00"
+  ];
+
+
+
+
+
