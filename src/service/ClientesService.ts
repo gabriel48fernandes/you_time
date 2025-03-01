9ec9582badaad79fbc459c215d29b7fca1285882
gabriel48fernandes <gabrielfernandesacdc@gmail.com>
2025-02-28 21:38:59 -0300

    Update ClientesService.ts

--- src/service/ClientesService.ts
+++ src/service/ClientesService.ts
@@ -30,9 +30,7 @@ export class ClienteService {
         let cliente = await this.buscarCliente(cod);
 
         if (cliente) {
-            console.log(" Cliente encontrado: Nome: " + cliente.nome + "Número:" + cliente.numero);
-
-
+            
             // Atualiza os campos do objeto cliente somente se o usuário digitar algo
             cliente.nome = novoNome || cliente.nome;
             cliente.numero = novoNumero || cliente.numero;
